96047ba358faedd1147d60d4a118b62f9966cf1e
Shane Tomlinson <set117@yahoo.com>
2012-01-02 20:44:39 +0000

    Finding the relay based on the data.url and origin.  Forcing it to be <origin>/relay

--- client/static/scripts/loader.js
+++ client/static/scripts/loader.js
@@ -1,8 +1,16 @@
 TestSwarm.Loader = (function() {
   "use strict";
 
+  function extractOrigin(url) {
+    if (!/^https?:\/\//.test(url)) url = window.location.href;
+    var m = /^(https?:\/\/[-_a-zA-Z\.0-9:]+)/.exec(url);
+    if (m) return m[1];
+    return url;
+  }
+
   function load(data, callback) {
-    var testWindow = WinChan.open(data.url, "http://testmob.org/relay.html", "", {}, function(err, data) {
+    var relay = extractOrigin(data.url) + "/relay";
+    var testWindow = WinChan.open(data.url, relay, "", {}, function(err, data) {
       testWindow.close();
       callback(err, data);
     });
