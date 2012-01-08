12ec657a0e637f7edd9992284912e6f044c892ea
Shane Tomlinson <set117@yahoo.com>
2012-01-08 00:35:57 +0000

    Starting to focus on the family.

--- server/views.js
+++ server/views.js
@@ -1,8 +1,8 @@
 const ROUTES = {
-  '/index.html': index,
-  '/': index,
-  '/initiator': initiator,
-  '/test_runner': test_runner
+  'GET index.html': redirect_to_index,
+  'GET ': index,
+  'POST join_family': join_family,
+  'GET family/:family_name': family
 };
 
 function render(req, res, template, data) {
@@ -13,23 +13,36 @@ function render(req, res, template, data) {
   res.render(template, data);
 }
 
+function redirect_to_index(req, res) {
+  // perm redirect to /
+  res.redirect("/", 301);
+}
+
 function index(req, res) {
   render(req, res, "index.ejs");
 }
 
-function initiator(req, res) {
-  render(req, res, "initiator.ejs", { title: "Initiate a Test" });
+function join_family(req, res) {
+  var family_name = req.body.family_name;
+  req.session.family_name = family_name;
+  res.redirect("/family/" + family_name);
 }
 
-function test_runner(req, res) {
-  render(req, res, "test_runner.ejs", { title: "Wait for Tests" });
+function family(req, res) {
+  var family_name = req.params.family_name;
+
+  render(req, res, "family.ejs", { title: family_name + " Family" });
 }
 
 function init(config) {
   var app = config.app;
 
   for(var key in ROUTES) {
-    app.get(key, ROUTES[key]);
+    var parts = key.split(" "),
+        method = parts[0].toLowerCase(),
+        url = "/" + (parts[1] || "");
+
+    app[method](url, ROUTES[key]);
   }
 }
 
